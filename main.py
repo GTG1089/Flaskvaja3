d86e8623896a5816c4c45deb28cf77bae3eeb426
GTG1089 <gabbymozart2@gmail.com>
2026-02-24 07:31:15 +0000

    5

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 app.run(debug=True)
+@app.route('/coinFlip')
+def coinFlip():
+    return render_template('index.html')
+@app.route('/coinData')
+def coinData():
+    rnd = random.randint(0,1)
+    return ["TAILS", "HEADS"][rnd]
