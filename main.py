f32d5519ab4511c20c690b76c9ea7a27188ef8e7
GTG1089 <gabbymozart2@gmail.com>
2026-02-25 09:48:42 +0000

    7

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 @app.route('/')
@@ -10,8 +11,16 @@ def coinFlip():
     return render_template('index.html')
 @app.route('/coinData')
 def coinData():
+    print(request.args["vrednost"])
     rnd = random.randint(0,1)
-    return ["TAILS", "HEADS"][rnd]
+    if rnd == 1:
+        status = "HEADS"
+        img_url = "https://i.postimg.cc/CBNJNfDJ/heads.png" 
+    else:
+        status = "TAILS"
+        img_url = "https://i.postimg.cc/zysdXN8w/tails.png"
+    status=["TAILS", "HEADS"][rnd]
+    return {"img":img_url, "status": status}
 if __name__ == '__main__':
     app.run(debug=True)
  
